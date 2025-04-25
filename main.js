7f9a681486d6115b205395b1804971f13c8b7b17
Njsc99 <nicolasj.saezc@gmail.com>
2025-04-25 00:10:55 -0400

    avance simulador blackjack

--- main.js
+++ main.js
@@ -1,17 +1,57 @@
-//Variable
+let jugador;
+let puntaje;
+let cartas;
+let carta;
+const cartasPorJugador = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"];
+const cartasJugador = [];
+function generarCarta() {
+    let carta = Math.floor(Math.random() * cartasPorJugador.length);
+    return cartasPorJugador[carta];
+}
 
-//Declaracion
-let cliente;
+console.log(generarCarta());
 
-cliente = "Juan Perez";
-//Declaracion de variables
+function ObtenerValor(carta) {
+    if (carta == "A") {
+        return A1 = 1, A2 = 11;
+    } else if (carta == "J" || carta == "Q" || carta == "K") {
+        return 10;
+    } else {
+        return carta;
+    }
+}
 
-console.log(cliente);
+console.log(ObtenerValor("A"));
+console.log(A1);
+console.log(A2);
 
-let edad = 30;
-console.log(edad);
+function comenzarJuego() {
+  jugador = prompt("Bienvenido al juego de BlackJack, ¿cuál es tu nombre?");
+  console.log(jugador + ", Vamos a comenzar el juego de BlackJack");
+  console.log("Recibirás 2 cartas, y el objetivo es sumar 21 puntos o lo más cercano a 21 sin pasarse.");
+  console.log("Las cartas son: " + cartasPorJugador.join(", "));
+  for (let i = 0; i < 2; i++) {
+    let carta = generarCarta();
+    cartasJugador.push(carta);
+    console.log("Tu carta" + " " +(i+1) +" "+ "es: " + carta);
+  }
+  if (cartasJugador.includes("A")) {
+    ObtenerValor("A");
+    console.log("El valor de tu carta es: " + A1 + " o " + A2); 
+    console.log("el A puede valer 1 o 11, ¿cual prefieres?");
+    let respuesta = prompt("Escribe 1 o 11");
+    if (respuesta == 1) {
+      console.log("El valor de tu carta A es: " + A1);
+      if (cartasJugador[0] != "A") {
+        puntaje = ObtenerValor(cartasJugador[0]) + A1;
+        console.log("Tu puntaje es: " + puntaje);
+      } else {
+        puntaje = ObtenerValor(cartasJugador[0]) + A1;
+        console.log("Tu puntaje es: " + puntaje);
+      }
+    }
+  }
 
-// Constantes: no se pueden cambiar
+}
 
-const nacimiento = 1993;
-console.log(nacimiento);
+comenzarJuego();
