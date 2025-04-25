f5751b24199633d7233c4ac5ab6ecc982c6f0809
Njsc99 <nicolasj.saezc@gmail.com>
2025-04-25 00:10:45 -0400

    avance simulador blackjack

---
+++ main.js
@@ -0,0 +1,17 @@
+//Variable
+
+//Declaracion
+let cliente;
+
+cliente = "Juan Perez";
+//Declaracion de variables
+
+console.log(cliente);
+
+let edad = 30;
+console.log(edad);
+
+// Constantes: no se pueden cambiar
+
+const nacimiento = 1993;
+console.log(nacimiento);
